2f3a1ee93308cc406e8d8d1313fa937d2ae8a30b
matinnili <matinnili94@gmail.com>
2025-06-05 03:47:54 +0000

    final update

--- locustfile.py
+++ locustfile.py
@@ -6,14 +6,7 @@ class QuickstartUser(HttpUser):
 
     @task
     def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+        self.client.post("/api/v1/whisper", files={"file": ("test.mp3", open("test.mp3", "rb"), "audio/mp3")})
 
-    @task(3)
-    def view_items(self):
-        for item_id in range(10):
-            self.client.get(f"/item?id={item_id}", name="/item")
-            time.sleep(1)
 
-    def on_start(self):
-        self.client.post("/login", json={"username":"foo", "password":"bar"})
+
