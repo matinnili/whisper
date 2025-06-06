1f3aafaac00763805d64115e5d3431c23789d21f
matinnili <matinnili94@gmail.com>
2025-06-06 07:09:58 +0000

    change the model to turbo

--- celery_worker.py
+++ celery_worker.py
@@ -6,7 +6,7 @@ app = Celery("worker", broker="redis://redis:6379/0",backend="redis://redis:6379
 
 @app.task
 def transcribe_audio(filepath):
-    model = whisper.load_model("base").to("cuda")
+    model = whisper.load_model("turbo").to("cuda")
     print(f"----------------- hi i'm here")
     result = model.transcribe(filepath)
     
