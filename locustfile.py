f7f0ee92c1f635f5936427bb3a8c9c6cfdb292f6
Your Name <you@example.com>
2025-06-07 11:07:20 +0000

    update

--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,15 @@
 import time
 from locust import HttpUser, task, between
+import logging as log
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2.5)
 
     @task
     def hello_world(self):
-        self.client.post("/api/v1/whisper", files={"file": ("test.mp3", open("/mnt/locust/test.mp3", "rb"), "audio/mp3")})
+        result=self.client.get("/api/v1/whisper")
+        log.info(f"Response: {result.status_code} - {result.text}")
+
 
 
 
--- main.py
+++ main.py
@@ -16,20 +16,21 @@ app.add_middleware(
 os.environ["PYTORCH_CUDA_ALLOC_CONF"]="expandable_segments:True"
 
 
-@app.post("/api/v1/whisper")
-async def transcript(file : UploadFile):
+@app.get("/api/v1/whisper")
+async def transcript():
 
     # file=file.file
     
-    print(type(file))
-    stt = whisper.load_model("turbo").to("cuda")
+    # print(type(file))
+    
     cwd=os.getcwd()
     path=os.path.join(cwd, "temp.wav")
-    with open(path, "wb") as f:
-        f.write(await file.read())
+    # with open(path, "wb") as f:
+    #     f.write(await file.read())
     
     start_time=time.datetime.now()
-    result=stt.transcribe(path,language="fa")
+    result=await transcribe(path)
+    
     # if os.path.exists(path):
     #     os.remove(path)
         
@@ -40,3 +41,7 @@ async def transcript(file : UploadFile):
     print(f"Transcription completed in {end_time - start_time} seconds")
     return text
 
+async def transcribe(path):
+    stt = whisper.load_model("large").to("cuda")
+    result=stt.transcribe(path,language="fa")
+    return result
