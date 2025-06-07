7691ec0e5b2ae104511269f10784046c311d69f0
Your Name <you@example.com>
2025-06-07 11:47:39 +0000

    update

--- main.py
+++ main.py
@@ -15,28 +15,32 @@ app.add_middleware(
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"]="expandable_segments:True"
 
-
-@app.post("/api/v1/whisper")
+app.prefix="/api/v1/whisper"
+@app.post("/api/v1/whisper/audio/transcriptions")
 async def transcript(file : UploadFile):
 
     # file=file.file
     
     print(type(file))
+
     stt = whisper.load_model("turbo").to("cuda")
     cwd=os.getcwd()
+    start_time=time.datetime.now()
     path=os.path.join(cwd, "temp.wav")
     with open(path, "wb") as f:
         f.write(await file.read())
-    
+    end_time=time.datetime.now()
+    print(f"format conversion completed in {end_time - start_time} seconds")
     start_time=time.datetime.now()
     result=stt.transcribe(path,language="fa")
-    # if os.path.exists(path):
-    #     os.remove(path)
+    if os.path.exists(path):
+        os.remove(path)
         
     
     text = result["text"].strip()
     end_time=time.datetime.now()
     total_time= end_time - start_time
     print(f"Transcription completed in {end_time - start_time} seconds")
-    return text
+    return {
+        "text": text}
 
